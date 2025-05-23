aa21b814a2e0a778f7331906408e77361d8a6333
Azamat Suleimenov <aza517@mail.ru>
2025-05-23 21:36:25 +0500

    сделал header

---
+++ front/src/page/body.tsx
@@ -0,0 +1,17 @@
+
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="body__box">
+                <div className="body__box__home">
+                    <div className="__box__home__image">sdasd</div>
+                    <div>sdasd</div>
+                </div>
+                <div>asdasd</div>
+            </div>
+        </div>
+    )
+}
+
+export default Body
